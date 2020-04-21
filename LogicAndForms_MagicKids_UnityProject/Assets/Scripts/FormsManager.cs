d49208dd14b0354088588d15408a4a55ec5afdd4
tomashined <45771896+tomashined@users.noreply.github.com>
2020-04-21 23:59:27 +0300

    Appering of Forms and Buttons

---
+++ LogicAndForms_MagicKids_UnityProject/Assets/Scripts/FormsManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FormsManager : MonoBehaviour
+{
+    const int formsCount = 9;
+    public Slider slider;
+    public Transform FormHolder;
+    public GameObject[] formsArray = new GameObject[formsCount];
+
+
+    void Start()
+    {
+        slider.maxValue = formsCount;
+        slider.value = 0;
+        FormAppear((int)slider.value);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void NextButtonPressed()
+    {
+        slider.value += slider.maxValue / formsCount;
+        FormAppear((int)slider.value);
+    }
+
+    public void BackButtonPressed()
+    {
+        slider.value -= slider.maxValue / formsCount;
+        FormAppear((int)slider.value);
+    }
+
+    void FormAppear(int index)
+    {
+        if (index < 0 || index >= formsCount) return;
+        if (FormHolder.childCount > 0) foreach (Transform child in FormHolder) Destroy(child.gameObject);
+        GameObject Form = Instantiate(formsArray[index], FormHolder);
+    }
+}
