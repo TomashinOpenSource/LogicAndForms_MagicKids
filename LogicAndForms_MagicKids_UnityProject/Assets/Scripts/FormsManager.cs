cfd5c9110d67a1975712ceb81573904810434b5b
tomashined <45771896+tomashined@users.noreply.github.com>
2020-04-22 14:14:36 +0300

    DotWeenAnimation

--- LogicAndForms_MagicKids_UnityProject/Assets/Scripts/FormsManager.cs
+++ LogicAndForms_MagicKids_UnityProject/Assets/Scripts/FormsManager.cs
@@ -7,8 +7,9 @@ public class FormsManager : MonoBehaviour
 {
     const int formsCount = 9;
     public Slider slider;
-    public Transform FormHolder;
+    public Transform FormHolder, EffectHolder;
     public GameObject[] formsArray = new GameObject[formsCount];
+    public GameObject[] effectsArray;
 
 
     void Start()
@@ -40,6 +41,8 @@ public class FormsManager : MonoBehaviour
     {
         if (index < 0 || index >= formsCount) return;
         if (FormHolder.childCount > 0) foreach (Transform child in FormHolder) Destroy(child.gameObject);
+        if (EffectHolder.childCount > 0) foreach (Transform child in EffectHolder) Destroy(child.gameObject);
         GameObject Form = Instantiate(formsArray[index], FormHolder);
+        GameObject Effect = Instantiate(effectsArray[Random.Range(0, effectsArray.Length)], EffectHolder);
     }
 }
